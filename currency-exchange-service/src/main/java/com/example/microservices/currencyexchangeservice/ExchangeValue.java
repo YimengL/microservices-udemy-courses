8bd6ebeb8be2f1628d2500e9425d91f4847e3044
YimengL <yimengcsu@gmail.com>
2020-03-07 18:35:04 -0600

    show port

--- currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/ExchangeValue.java
@@ -1,13 +1,25 @@
 package com.example.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+
+@Entity
 public class ExchangeValue {
 
+  @Id
   private Long id;
+
+  @Column(name ="currency_from")
   private String from;
+
+  @Column(name = "currency_to")
   private String to;
+
   private BigDecimal conversionMultiple;
+  private int port;
 
   public ExchangeValue() {}
 
@@ -32,5 +44,13 @@ public class ExchangeValue {
 
   public BigDecimal getConversionMultiple() {
     return conversionMultiple;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
+  }
+
+  public int getPort() {
+    return port;
   }
 }
